0e81d4fdce21f22719cf9db4eb839c3a6456d8dd
Lina Torovoltas <193523522+lina-torovoltas@users.noreply.github.com>
2026-08-21 17:34:29 +0500

    small fox

--- Zig/pentagon.zig
+++ Zig/pentagon.zig
@@ -1,27 +1,29 @@
+// zig version 0.16.0
+
 const std = @import("std");
+const print = std.debug.print;
 
 
 
 pub fn main(init: std.process.Init) !void {
     const io = init.io;
-    const rng_impl: std.Random.IoSource = .{ .io = io };
-    const rand = rng_impl.interface();
+    const rand = (std.Random.IoSource{ .io = io }).interface();
     
-    var progress: u64 = 0;
+    var progress: u7 = 0;
     
     while (progress < 100) {
         const max = @min(progress + 20, 100);
-        progress = rand.intRangeAtMost(u64, progress + 1, max);
+        progress = rand.intRangeAtMost(u7, progress + 1, max);
         
-        std.debug.print("Pentagon hack progress: {}%\n", .{progress});
+        print("Pentagon hack progress: {}%\n", .{progress});
         
         try io.sleep(.fromSeconds(1), .awake);
     }
 
-    if (rand.intRangeAtMost(u8, 1, 30) > 20) {
-        std.debug.print("Pentagon hack: Completed successfully.\n", .{});
+    if (rand.intRangeAtMost(u5, 1, 30) > 20) {
+        print("Pentagon hack: Completed successfully.\n", .{});
         
     } else {
-        std.debug.print("Pentagon hack: Failed.\n", .{});
+        print("Pentagon hack: Failed.\n", .{});
     }
 }
